eb97ace62ed461166e3ad9be5aef023b1f28e3fc
Afzal <afzalashraf814@gmail.com>
2023-03-03 20:50:30 +0500

    initial commit with some configurations

---
+++ android/app/src/main/kotlin/com/example/chatgpt_app_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chatgpt_app_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const Color cPrimaryBlue = Color(0xff3369ff);
+const Color cWhite = Color(0xFFFFFFFF);
+const Color cGrey = Color(0xfff4f4f4);
+const Color cBlack = Color(0xff3f3f3f);
+
+final eRegular = GoogleFonts.nunito(
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+final eSemiBold = GoogleFonts.nunito(
+  fontWeight: FontWeight.w600,
+);
+
+final eBold = GoogleFonts.nunito(
+  fontWeight: FontWeight.w800,
+  fontSize: 17,
+);
